b21f3696729c4dc927471447c223bd03362be0cf
Sylvain Pendino <s.pendino@criteo.com>
2024-10-06 21:50:06 +0200

    Fix clip_array
    To clips can now have the same position

--- test/tracks/test_clip_array.lua
+++ test/tracks/test_clip_array.lua
@@ -22,7 +22,7 @@ function TestClipArray:testAddClipWithSameType()
   local array = ClipArray(t)
   local clip = Clip(1, 23, t)
   array:addClip(clip)
-  luaunit.assertEquals(array.values[1], clip)
+  luaunit.assertEquals(array.values[1].clips[1], clip)
 end
 
 function TestClipArray:testAddClipWithDifferentType()
@@ -41,4 +41,16 @@ function TestClipArray:testGetIntersectingClips()
   array:addClip(clip)
   luaunit.assertEquals(array:getIntersectingClips(20)[1], clip)
   luaunit.assertEquals(#array:getIntersectingClips(4), 0)
+end
+
+function TestClipArray:testGetIntersectingClipsWithTwoAlignedClips()
+  local t = 'some_type'
+  local array = ClipArray(t)
+  local clip1 = Clip(5, 23, t)
+  local clip2 = Clip(5, 21, t)
+  array:addClip(clip1)
+  array:addClip(clip2)
+  local intersecting = array:getIntersectingClips(20)
+  luaunit.assertEquals(intersecting[1], clip1)
+  luaunit.assertEquals(intersecting[2], clip2)
 end
